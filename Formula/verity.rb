35a38440b12b6b3c429b8f7c9748fd31189f5093
Axel Mauroy <45535819+axel-mauroy@users.noreply.github.com>
2026-05-07 13:53:57 +0200

    chore(brew): update formulas for v0.2.5 and fix CD workflow (#3)

--- Formula/verity.rb
+++ Formula/verity.rb
@@ -1,9 +1,9 @@
 class Verity < Formula
   desc "Hexagonal Data Contract & Transformation Engine"
   homepage "https://github.com/axel-mauroy/verity-governance-as-code"
-  url "https://github.com/axel-mauroy/verity-governance-as-code/releases/download/v0.1.0/verity-macos-universal"
-  sha256 "580929494243f221783dbe434bef1957026346e559dd1ad37c8a8b23f7df94a4"
-  version "0.1.0"
+  url "https://github.com/axel-mauroy/verity-governance-as-code/releases/download/v0.2.5/verity-macos-universal"
+  sha256 "e44834643bd91d57e89d33f7968eb0bbe51add4649b2b3f859af7737e3cd285c"
+  version "0.2.5"
 
   def install
     bin.install "verity-macos-universal" => "verity"
