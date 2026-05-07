b8addd51947b69a510068a9618207a1f4a74c8c1
Axel Mauroy <axel.mauroy@gmail.com>
2026-05-07 13:16:27 +0200

    chore(brew): update formulas for v0.2.5 and fix CD workflow

--- Formula/verity-bigquery.rb
+++ Formula/verity-bigquery.rb
@@ -1,9 +1,9 @@
 class VerityBigquery < Formula
   desc "BigQuery Connector for Verity Governance Engine"
   homepage "https://github.com/axel-mauroy/verity-governance-as-code"
-  url "https://github.com/axel-mauroy/verity-governance-as-code/releases/download/v0.1.0/verity-bigquery-macos-universal"
-  sha256 "REPLACE_WITH_ACTUAL_SHA_DURING_RELEASE"
-  version "0.1.0"
+  url "https://github.com/axel-mauroy/verity-governance-as-code/releases/download/v0.2.5/verity-bigquery-macos-universal"
+  sha256 "2e9a11e20b1fb29d6ec339603c4d72700868e4b0661f6db3c7b9f979ed38ba92"
+  version "0.2.5"
 
   depends_on "verity"
 
