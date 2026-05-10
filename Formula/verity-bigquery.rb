e98166aa14c92c00fb850ca5b589515f4ab67437
axel-mauroy <45535819+axel-mauroy@users.noreply.github.com>
2026-05-10 18:09:44 +0000

    chore(brew): update formulas for v0.2.8

--- Formula/verity-bigquery.rb
+++ Formula/verity-bigquery.rb
@@ -2,7 +2,7 @@ class VerityBigquery < Formula
   desc "BigQuery Connector for Verity Governance Engine"
   homepage "https://github.com/axel-mauroy/verity-governance-as-code"
   url "https://github.com/axel-mauroy/verity-governance-as-code/releases/download/v0.2.8/verity-bigquery-macos-universal"
-  sha256 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  sha256 "9772b46867570839a4a6b68453cd4eb5e50947aff15b38a09f9265a92db7fa7a"
 
   depends_on "axel-mauroy/verity-governance-as-code/verity"
 
--- Formula/verity.rb
+++ Formula/verity.rb
@@ -1,8 +1,8 @@
 class Verity < Formula
   desc "Hexagonal Data Contract & Transformation Engine"
   homepage "https://github.com/axel-mauroy/verity-governance-as-code"
-  url "https://github.com/axel-mauroy/verity-governance-as-code/releases/download/v0.2.5/verity-macos-universal"
-  sha256 "e44834643bd91d57e89d33f7968eb0bbe51add4649b2b3f859af7737e3cd285c"
+  url "https://github.com/axel-mauroy/verity-governance-as-code/releases/download/v0.2.8/verity-macos-universal"
+  sha256 "c5eb7e0309c4ded3350af3add40d522d782059670e10bd25229043782dcd1098"
 
   def install
     bin.install "verity-macos-universal" => "verity"
